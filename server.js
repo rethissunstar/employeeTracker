094875380c231acd0702bba821b903bf8c49aae3
rethissunstar <rethissunstar@gmail.com>
2023-06-17 13:12:00 -0400

    add role section added with a recognition of existent dept

--- server.js
+++ server.js
@@ -1,44 +1,3 @@
-
-// const inquirer = require('inquirer');
-// const express = require('express');
-// // Import and require mysql2 and inquirer
-// const mysql = require('mysql2');
-
-
-// const PORT = process.env.PORT || 3001;
-// const { default: Choices } = require('inquirer/lib/objects/choices');
-// const app = express();
-
-// // Express middleware
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-
-// // Connect to database
-// const db = mysql.createConnection(
-//   {
-//     host: 'localhost',
-//     // MySQL username,
-//     user: 'root',
-//     // TODO: Add MySQL password
-//     password: 'theDragon@123',
-//     database: 'employeeTracker_db'
-//   },
-//   console.log(`Connected to the employeeTracker database.`)
-// );
-
-// inquirer
-//   .prompt([
-//     {
-//         type: 'checkbox',
-//         message: 'What would you like to do? ',
-//         name: 'Main Choice',
-//         choices: ['View', 'Add', 'Update']
-
-//     }
-//   ])
-//   .then((response) =>
-//     console.log(response));
-  
 const inquirer = require('inquirer');
 const express = require('express');
 const mysql = require('mysql2');
@@ -58,11 +17,11 @@ const db = mysql.createConnection({
   database: 'employeeTracker_db'
 });
 
+let startLoop = 0;
+
+// for (startLoop = 0; startLoop < 1;){
+
 db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database: ' + err.stack);
-    return;
-  }
   console.log('Connected to the employeeTracker database.');
 
   inquirer
@@ -71,11 +30,15 @@ db.connect((err) => {
         type: 'checkbox',
         message: 'Employee Tracker: What would you like to do?',
         name: 'MainChoice',
-        choices: ['View', 'Add', 'Update']
+        choices: ['View', 'Add', 'Update', 'exit']
       }
     ])
     .then((response) => {
       console.log(response);
+      if (response.MainChoice.includes('exit')) {
+        startLoop = 1;
+        db.end();
+      }
 
       if (response.MainChoice.includes('View')) {
         console.log('You chose to view');
@@ -98,7 +61,7 @@ db.connect((err) => {
                 }
                 console.log('Departments:');
                 console.table(results);
-                //db.end(); // Close the database connection after displaying the departments
+                db.end();
               });
             }
             if (response.ViewChoice.includes('Employees')) {
@@ -110,7 +73,7 @@ db.connect((err) => {
                 }
                 console.log('Employees:');
                 console.table(results);
-                //db.end(); // Close the database connection after displaying the departments
+                db.end();
               });
             }
             if (response.ViewChoice.includes('Roles')) {
@@ -122,7 +85,7 @@ db.connect((err) => {
                 }
                 console.log('Roles:');
                 console.table(results);
-                //db.end(); // Close the database connection after displaying the departments
+                db.end();
               });
             }
           });
@@ -158,50 +121,102 @@ db.connect((err) => {
                       return;
                     }
                     console.log('Department added successfully!');
+                    db.end();
                   });
                 });
             }
             if (response.AddChoice.includes('Employees')) {
               console.log('You chose to add to Employees');
               inquirer
-              .prompt([
-                {
-                  type: 'input',
-                  message: 'What is the first name of the employee?',
-                  name: 'firstName'
-                },
-                {
-                  type: 'input',
-                  message: 'What is the last name of the employee?',
-                  name: 'lastName'
-                },
-                {
-                  type: 'input',
-                  message: 'What is the role id of the employee?',
-                  name: 'role_id'
-                },
-                {
-                  type: 'input',
-                  message: 'Who is the manager of the employee?',
-                  name: 'manager'
-                }
-              ])
-              .then((response) => {
-                console.log(response);
-                const { firstName, lastName, role_id, manager } = response;
-                const values = [firstName, lastName, role_id, manager];
-                db.query('INSERT INTO employee_data (firstName, lastName, role_id, manager) VALUES (?, ?, ?, ?)', values, (err, results) => {
-                  if (err) {
-                    console.log('Error occurred while inserting into departments: ' + err);
-                    return;
+                .prompt([
+                  {
+                    type: 'input',
+                    message: 'What is the first name of the employee?',
+                    name: 'firstName'
+                  },
+                  {
+                    type: 'input',
+                    message: 'What is the last name of the employee?',
+                    name: 'lastName'
+                  },
+                  {
+                    type: 'input',
+                    message: 'What is the role id of the employee?',
+                    name: 'role_id'
+                  },
+                  {
+                    type: 'input',
+                    message: 'Who is the manager of the employee?',
+                    name: 'manager'
                   }
-                  console.log('Department added successfully!');
+                ])
+                .then((response) => {
+                  console.log(response);
+                  const { firstName, lastName, role_id, manager } = response;
+                  const values = [firstName, lastName, role_id, manager];
+                  db.query(
+                    'INSERT INTO employee_data (firstName, lastName, role_id, manager) VALUES (?, ?, ?, ?)',
+                    values,
+                    (err, results) => {
+                      if (err) {
+                        console.log('Error occurred while inserting into Employee_data: ' + err);
+                        return;
+                      }
+                      console.log('Employee added successfully!');
+                    }
+                  );
                 });
-              });
             }
             if (response.AddChoice.includes('Roles')) {
               console.log('You chose to add to Roles');
-              // Code to add roles
+
+              db.query('SELECT id, name FROM department', function (error, results) {
+                if (error) {
+                  console.error(error);
+                  db.end();
+                  return;
+                }
+
+                const departmentList = results.map((result) => ({ name: result.name, value: result.id }));
+                console.log(departmentList);
+
+                inquirer
+                  .prompt([
+                    {
+                      type: 'input',
+                      message: 'What is the job Title?',
+                      name: 'job_title'
+                    },
+                    {
+                      type: 'checkbox',
+                      message: 'What is the department id this job belongs?',
+                      name: 'dept_id',
+                      choices: departmentList
+                    },
+                    {
+                      type: 'input',
+                      message: 'What is the salary for this role',
+                      name: 'salary'
+                    }
+                  ])
+                  .then((response) => {
+                    console.log(response);
+                    const { job_title, dept_id, salary } = response;
+                    const values = [job_title, dept_id, salary];
+                    db.query(
+                      'INSERT INTO roles (job_title, dept_id, salary) VALUES (?, ?, ?)',
+                      values,
+                      (err, results) => {
+                        if (err) {
+                          console.log('Error occurred while inserting into roles: ' + err);
+                          return;
+                        }
+                        console.log('role added successfully!');
+                        db.end();
+                      }
+                    );
+                  });
+              });
             }
           });
       }
@@ -213,8 +228,12 @@ db.connect((err) => {
     })
     .catch((err) => {
       console.error('Error occurred while prompting: ' + err);
+      db.end();
     });
 });
+
+//}
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
